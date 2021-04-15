95f2bbc0cfaa24433da409121ade5b1dd9fa7920
Phongtran <tanphongtr@gmail.com>
2021-04-15 17:32:03 +0700

    push

--- app/models/post.py
+++ app/models/post.py
@@ -4,9 +4,11 @@ from app.models import User
 # from django.contrib.auth.models import User
 
 class Post(models.Model):
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    sid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255,)
+    content = models.TextField(max_length=1000, blank=True, default='', )
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, )
+    update_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now=True)
 
 
