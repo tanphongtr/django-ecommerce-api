cb6a683e19afa8d45c95e3373664978259ac373d
Phongtran <tanphongtr@gmail.com>
2021-04-15 15:38:39 +0700

    Setup for heroku

--- django_api/settings.py
+++ django_api/settings.py
@@ -329,3 +329,8 @@ EMAIL_HOST_PASSWORD = 'ucnditvoiovkzawu'  # past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
+
+
+# For heroku.com
+import django_heroku
+django_heroku.settings(locals())
