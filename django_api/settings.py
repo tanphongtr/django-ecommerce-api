d2984ee77c3caae9a73fd4b6e2e5b7b482c56588
Phongtran <tanphongtr@gmail.com>
2021-04-15 16:59:36 +0700

    push

--- django_api/settings.py
+++ django_api/settings.py
@@ -98,10 +98,11 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
