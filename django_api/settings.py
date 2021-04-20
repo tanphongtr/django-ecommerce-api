63a9ed3fa4fe9da8015c445f8805508729938d39
Phongtran <tanphongtr@gmail.com>
2021-04-21 00:43:03 +0700

    update

--- django_api/settings.py
+++ django_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'vn5iga)cc=i6n$w&zz45u)$@(#c&oal%77w77b8y0b)bs1o#t6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
