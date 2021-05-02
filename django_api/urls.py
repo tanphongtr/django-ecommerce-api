563790cb15d54880aa8aa7648856d0960243a4fd
Phongtran <tanphongtr@gmail.com>
2021-05-03 00:52:03 +0700

    Up code

--- django_api/urls.py
+++ django_api/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.views.debug import default_urlconf
 from django.contrib import admin
 from django.urls import path, re_path, include
@@ -80,7 +79,6 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls'))
     path('api/', include('api.urls')),
     path('download/<uuid:sid>/', file_downloading),
-    path('__debug__/', include(debug_toolbar.urls)),
 ] 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # For URL media / files
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
